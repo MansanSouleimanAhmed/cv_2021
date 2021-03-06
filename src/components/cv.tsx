29536e07a7504ffd546e428623f9edc6881f53f6
MansanSouleimanAhmed <msa.github.cv@gmail.com>
2021-03-06 23:45:13 +0100

    cv animation

--- src/components/cv.tsx
+++ src/components/cv.tsx
@@ -3,9 +3,37 @@ import { useSpring, animated, useTrail } from 'react-spring';
 import Header from '../header/header';
 
 const Main: React.FC = () => {
+	// @ts-ignore
+	const trans = (x) => `translateX(-${x}px)`;
+	const items = [
+		'I AM A FRONT-END DEVELOPER-REACTJS_I AM A FRONT-END DEVELOPER-REACTJS_',
+		'I AM A FRONT-END DEVELOPER-REACTJS_I AM A FRONT-END DEVELOPER-REACTJS_',
+	];
+	const [trail, set] = useTrail(items.length, () => ({
+		x: 0,
+		config: { mass: 2, tension: 200, friction: 50 },
+	}));
+
 	return (
-		<div className="container">
+		<div
+			className="container"
+			// @ts-ignore
+			onMouseMove={(e) => set({ x: e.clientX })}
+		>
 			<Header />
+			<div className="text-animation">
+				{trail.map((props, index) => (
+					<animated.p
+						key={index}
+						style={{
+							// @ts-ignore
+							transform: props.x.interpolate(trans),
+						}}
+					>
+						{items}
+					</animated.p>
+				))}
+			</div>
 		</div>
 	);
 };
